704dccafe798953e2a1d6625857d0a4069ae16ba
Anthony Freda <anthonyfreda323@gmail.com>
2019-05-22 17:09:54 -0400

    added linter

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import logo from '../../logo.svg'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h3>Heartbeat</h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-      </header>
-    </div>
-  )
-}
+const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <h3>Heartbeat</h3>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </header>
+  </div>
+)
 
 export default App
