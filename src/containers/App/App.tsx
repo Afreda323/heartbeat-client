da2de119e96e6e8ff153ab240b58705b794d1bba
Anthony Freda <anthonyfreda323@gmail.com>
2019-05-22 16:54:51 -0400

    moved files around

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../../logo.svg'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Heartbeat</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
